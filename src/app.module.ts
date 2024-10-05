0a867d3e903cc7631dbdc8c399c01ed242656e01
xander-che <achernys123@gmail.com>
2024-10-05 16:57:14 +0300

    mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RoomsModule } from './rooms/rooms.module';
 import { ScheduleModule } from './schedule/schedule.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { RoomsController } from './rooms/rooms.controller';
+import { ScheduleController } from './schedule/schedule.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [RoomsModule, ScheduleModule],
-	controllers: [AppController],
+	imports: [
+		ScheduleModule,
+		RoomsModule,
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot('mongodb://localhost/simple-airbnb-db'),
+	],
+	controllers: [AppController, RoomsController, ScheduleController],
 	providers: [AppService],
 })
 export class AppModule {}
