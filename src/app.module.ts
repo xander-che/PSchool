1aa405df2791ebc3c4018a0815cd94289daf885e
xander-che <achernys123@gmail.com>
2024-10-05 15:59:13 +0300

    mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoomsModule } from './rooms/rooms.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [],
+  imports: [RoomsModule, ScheduleModule],
   controllers: [AppController],
   providers: [AppService],
 })
