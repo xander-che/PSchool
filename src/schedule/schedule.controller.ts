5b93d8d360748ad86de8ffa9774eeeff79d3a5ab
xander-che <achernys123@gmail.com>
2024-10-09 20:35:24 +0300

    mongo controller

--- src/schedule/schedule.controller.ts
+++ src/schedule/schedule.controller.ts
@@ -1,9 +1,12 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { Schedule } from './models/schedule.model';
 import { FindEntryDto } from './dto/find-entry.dto';
+import { ScheduleService } from './schedule.service';
 
 @Controller('schedule')
 export class ScheduleController {
+	constructor(private readonly scheduleService: ScheduleService) {}
+
 	@Post('create')
 	async create(@Body() dto: Omit<Schedule, 'id'>) {}
 
