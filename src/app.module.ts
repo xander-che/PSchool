a3afa2470fc52c0f0aba4d6832b0ccff2e55f364
xander-che <achernys123@gmail.com>
2024-10-24 20:21:00 +0300

    mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,13 @@ import { ScheduleModule } from './schedule/schedule.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { getMongoConfig } from './rooms/configs/mongo.config';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
 	imports: [
 		ScheduleModule,
 		RoomsModule,
 		ConfigModule.forRoot(),
-		MongooseModule.forRoot('mongodb://127.0.0.1:27017/simple-airbnb-db'),
 		MongooseModule.forRootAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
